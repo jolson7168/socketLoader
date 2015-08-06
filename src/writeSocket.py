8ec084939b872b1fa9e67f7e59c548ec61d17655
jolson7168 <technology@nododos.com>
2015-08-05 20:04:38 -0500

    added trace print

--- src/writeSocket.py
+++ src/writeSocket.py
@@ -8,9 +8,11 @@ import socket
 
  
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+x=0
 while True:
 	t=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	x=x+1
+	print(t+": "+str(x))
 	sock.sendto(t+","+str(random.randint(1, 10))+","+str(random.randint(1, 10))+","+str(random.randint(1, 10))+","+str(random.randint(1, 10))+"\n", (UDP_IP, UDP_PORT))
